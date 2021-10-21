6389c09d300a9fc04ff64abffa8c78d9dca33446
Vitalii <vognuk@gmail.com>
2021-10-21 21:10:36 +0300

    Project start architecture

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
